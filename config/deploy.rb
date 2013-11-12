185f11ebd06d3e51e5d333349c9d698a756ab122
ndereli <nazlidereli@gmail.com>
2013-11-11 19:21:28 -0800

    dahkfdh

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ namespace :deploy do
   desc 'Create database'
     task :create do
       on roles(:db) do
-        run "cd #{current_path} && bundle exec rake db:create RAILS_ENV=staging"
+        run "cd #{current_path}; rake db:create RAILS_ENV=staging"
       end
     end        
 
