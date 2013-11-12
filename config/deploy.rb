ce1f8c6bc1e95006ab1091f922beec37dbf29bff
ndereli <nazlidereli@gmail.com>
2013-11-11 18:26:50 -0800

    change in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,8 +41,8 @@ namespace :deploy do
       end        
       before :create, 'rvm:hook'
       before :create, 'bundler:install'
-  before "deploy:assets:precompile", "deploy:fix_bug_env"
- 
+  before "deploy:create", "deploy:fix_bug_env"
+  before "deploy:assets:precompile", "deploy:create"
   
   desc 'Restart application'
   task :restart do
