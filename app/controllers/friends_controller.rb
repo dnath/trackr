2a56f5f522f460c49b1dc78b1408eb9e58ca28ce
Dibyendu Nath <dev.nath.cs@gmail.com>
2013-11-18 14:14:15 -0800

    CSS 3

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -4,20 +4,27 @@ class FriendsController < ApplicationController
   def index
   	current_user = User.find(session[:current_user])
   	api = Koala::Facebook::API.new(session[:access_token])
-  	current_user_friends = api.get_object("/" + current_user.fb_id + "/?fields=friends.fields(id,name,picture)")
+  	current_user_friends = api.get_object("/" + current_user.fb_id + "/?fields=friends.fields(id)")
   	
     current_user_friends_in_trackr = []
     current_user_friend_ids = []
    
     current_user_friends['friends']['data'].each { |friend|
-      # puts friend['id'].to_s
-      current_user_friend_ids.push(friend['id']);
+      # puts 'friend = ' + friend.to_s
+      current_user_friend_ids << friend['id']
     }
     
     current_user_friends_in_trackr = User.find_all_by_fb_id(current_user_friend_ids)
-    @friends = current_user_friends_in_trackr
 
-    # puts "*************************************"
+    current_user_friends_fb_ids = []
+    current_user_friends_in_trackr.each { |f|
+      current_user_friends_fb_ids << f['fb_id']
+    }
+
+    # puts "current_user_friends_fb_ids = " + current_user_friends_fb_ids.to_s
+
+    @current_trackr_friends = api.get_objects(current_user_friends_fb_ids, :fields=>"first_name,last_name,picture").values
+    
     # @current_user_friends_in_trackr.each { |friend|
     #  puts friend.first_name.to_s
     # }
@@ -28,13 +35,4 @@ class FriendsController < ApplicationController
       format.xml { render xml: current_user_friends['friends']['data'] }
     end
   end
-
-  def user_image_url(user_id)
-    puts user_id
-    api = Koala::Facebook::API.new(session[:access_token])
-    # puts "/" + user_id.to_s + "/?fields=picture"
-    user_image_url = api.get_object("/" + User.find(user_id)['fb_id'].to_s + "/?fields=picture")
-    # puts "*****************************" + user_image_url['picture']['data']['url']
-    return user_image_url['picture']['data']['url']
-  end  
 end
