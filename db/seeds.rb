daeaebb1c1e1d38fdbea7c8c9e9184233ced3438
Divya <divya_sambasivan@yahoo.co.in>
2013-11-22 02:36:43 -0800

    some debugging

--- db/seeds.rb
+++ db/seeds.rb
@@ -105,6 +105,10 @@ for i in 0..number_goal_instances-1
     goal_instance = GoalInstance.create(
                 { start_date: st, end_date: et, cheer_ons:rand(users.length/2), is_complete: complete},
         )
+    puts "before goal"
     Goal.find(goals[rand(goals.length)].id).goal_instances.push(goal_instance) #make this an instance of a random goal
+    puts "after goal before user"
     User.find(users[rand(users.length)].id).goal_instances.push(goal_instance) #make this instance of a random user
+    puts "after user"
+    puts ""
 end 
