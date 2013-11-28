aa91f6f503ce6352c1f9e9b946d67b1c215678ee
Divya <divya_sambasivan@yahoo.co.in>
2013-11-28 12:29:23 -0800

    removed hash comments

--- app/assets/javascripts/goal_instances.js
+++ app/assets/javascripts/goal_instances.js
@@ -1,5 +1,3 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
 $('.input-large').click(function() {
   var checked; 
   if ($(this).is(':checked')) {
@@ -12,5 +10,5 @@ $('.input-large').click(function() {
       url: "/goal_instance/complete",
       data: { id: $(this).data('post-id'), checked: checked }
    });     
-});# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+});
 
